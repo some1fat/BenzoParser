09ec6be4af2df85bc0cd1aa909b637707283dd92
alexei_prudivus <pupu6oi74@gmail.com>
2018-11-03 20:27:53 +0200

    push-push

--- controller/parser.js
+++ controller/parser.js
@@ -2,16 +2,17 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 
 exports.cParser = function(req, res) {
-    if (req.body.url){
-        axios.get(req.body.url)
+    if (req.body.html){
+        axios.get(req.body.html)
             .then(function (response) {
-                const $ = cheerio.load(response);
-                res.send($('.panes').html());
+                //const $ = cheerio.load(response);
+                //res.send($('.panes').html());
+                res.send(response);
             })
             .catch(function (error) {
                 res.send(error);
             });
     } else {
-        res.send('Error. Request param "URL" is not present in HTTP request.');
+        res.send('Error. Request param "HTML" is not present in HTTP request.');
     }
 };
